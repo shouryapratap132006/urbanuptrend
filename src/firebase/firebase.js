92fd90f028eb8658847e61b5fdc8464fd679d34d
Shourya Pratap <shouryapratap@Shouryas-MacBook-Pro.local>
2025-07-21 10:23:27 +0530

    error resolved

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBHWx-hHgfzJ8Xo2z7uBPTr25R1nDlsxWs",
   authDomain: "urbanuptrend-ab545.firebaseapp.com",
   projectId: "urbanuptrend-ab545",
-  storageBucket: "urbanuptrend-ab545.appspot.com", 
+  storageBucket: "urbanuptrend-ab545.firebasestorage.app",
   messagingSenderId: "161860531402",
   appId: "1:161860531402:web:3b5b8591dfbb9078ef3033",
-  measurementId: "G-XDYTBY77FK",
+  measurementId: "G-XDYTBY77FK"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); 
+const analytics = getAnalytics(app);
